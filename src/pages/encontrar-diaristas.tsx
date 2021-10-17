710d6252d22f9c3e59e1fed4af78eed0cdf6057a
GabrielCM08 <gabriel.camposm@hotmail.com>
2021-10-17 16:49:02 -0300

    configuring search and pages

--- src/pages/encontrar-diaristas.tsx
+++ src/pages/encontrar-diaristas.tsx
@@ -4,6 +4,13 @@ import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import TextInput from "ui/components/inputs/TextInput/TextInput";
 import { TextInputMask } from "react-native-masked-text";
 import Button from "ui/components/inputs/Button/Button";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import {
+   FormContainer,
+   TextContainer,
+   ErrorText,
+   ResponseContainer,
+  } from "@styles/pages/encontrar-diaristas.styled";
 
 const EncontrarDiaristas: React.FC = () => {
   const [cep, setcep] = useState("");
@@ -17,6 +24,7 @@ const EncontrarDiaristas: React.FC = () => {
         }
       />
 
+    <FormContainer>
       <TextInputMask
         value={cep}
         onChangeText={setcep}
@@ -29,9 +37,20 @@ const EncontrarDiaristas: React.FC = () => {
           label: "Digite seu CEP",
         }}
       />
+
+      <ErrorText>Cep não econtrado</ErrorText>
+
       <Button mode={"contained"} style={{ marginTop: 32 }}>
         Buscar
       </Button>
+    </FormContainer>
+
+      <UserInformation 
+        name={'Gabriel'}
+        rating={4}
+        picture={'https://github.com/GabrielCM08.png'}
+        description={"Rio Grande do Sul"}
+      />
     </View>
   );
 };
